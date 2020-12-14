d9f9ef539c12eefc8f6ef10b0fb6b0b7022ac13a
Ahmed Seif <ahmed.am.seif@gmail.com>
2020-12-14 23:35:35 +0200

    Task 4 Web Development Fall 2020 AUFS

---
+++ index.js
@@ -0,0 +1,8 @@
+let notes = ["here you type your first name","here you type your last name",
+    "Here you should type your email and it must contain '@' symbol",
+    "Please click the Choose File button to upload your personal national ID to continue booking your plane",
+    "Please choose the origin place you're at", "please choose the destination", "Please specify a date",
+    "Please specify a time"]
+function helpNote(notenum){
+    alert(notes[notenum]);
+}
